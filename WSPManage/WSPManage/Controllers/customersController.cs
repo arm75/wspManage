6bc4d691bb2ede092ba0bddb6e99466cf575db95
Aaron Montgomery <aaronrobertmontgomery@gmail.com>
2017-06-26 02:16:22 -0500

    added CRUD for customers.

---
+++ WSPManage/WSPManage/Controllers/customersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WSPManage.Models;
+
+namespace WSPManage.Controllers
+{
+    public class customersController : Controller
+    {
+        private WSPManageContext db = new WSPManageContext();
+
+        // GET: customers
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.customers.ToListAsync());
+        }
+
+        // GET: customers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = await db.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Active,FirstName,MiddleName,LastName,SSN,EIN,ContractName,AlternateName,MailingAddress,MailingCity,MailingState,MailingZipcode,PhysicalAddress,PhysicalCity,PhysicalState,PhysicalZipcode,HomePhoneNumber,WorkPhoneNumber,CellPhoneNumber,AlternateNumber,Notes")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customers.Add(customer);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: customers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = await db.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Active,FirstName,MiddleName,LastName,SSN,EIN,ContractName,AlternateName,MailingAddress,MailingCity,MailingState,MailingZipcode,PhysicalAddress,PhysicalCity,PhysicalState,PhysicalZipcode,HomePhoneNumber,WorkPhoneNumber,CellPhoneNumber,AlternateNumber,Notes")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: customers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = await db.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            customer customer = await db.customers.FindAsync(id);
+            db.customers.Remove(customer);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WSPManage/WSPManage/Models/customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WSPManage.Models
+{
+    [Table("dbo.customers")]
+    public class customer
+    {
+        public int ID { get; set; }
+        public bool Active { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string SSN { get; set; }
+        public string EIN { get; set; }
+        public string ContractName { get; set; }
+        public string AlternateName { get; set; }
+        public string MailingAddress { get; set; }
+        public string MailingCity { get; set; }
+        public string MailingState { get; set; }
+        public string MailingZipcode { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PhysicalCity { get; set; }
+        public string PhysicalState { get; set; }
+        public string PhysicalZipcode { get; set; }
+        public string HomePhoneNumber { get; set; }
+        public string WorkPhoneNumber { get; set; }
+        public string CellPhoneNumber { get; set; }
+        public string AlternateNumber { get; set; }
+        public string Notes { get; set; }
+    }
+}
